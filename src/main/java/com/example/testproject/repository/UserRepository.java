94df3c54df3b75df3f6d17dabf05d652c6fc7ed6
Britt Brink <485247@student.fontys.nl>
2022-10-03 13:38:42 +0200

    crud applicatie

---
+++ src/main/java/com/example/testproject/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.testproject.repository;
+
+import com.example.testproject.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
