325e64438cf298646c8f8a0f882cd4d00376d206
Britt Brink <485247@student.fontys.nl>
2022-12-01 10:57:34 +0100

    crud applicatie done van de person, begonnen met uitlezen recipe

--- src/main/java/com/example/testproject/controller/RecipeController.java
+++ src/main/java/com/example/testproject/controller/RecipeController.java
@@ -4,10 +4,10 @@ import com.example.testproject.model.Person;
 import com.example.testproject.model.Recipes;
 import com.example.testproject.repository.RecipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -16,6 +16,29 @@ public class RecipeController {
 
     @Autowired
     private RecipeRepository recipeRepository;
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello World";
+    }
+
+    @GetMapping(value = "/testen/api")
+    private String GetTestApi(){
+        String uri="http://localhost:8080/hello";
+        RestTemplate restTemplate = new RestTemplate();
+        String result= restTemplate.getForObject(uri, String.class);
+        return result;
+    }
+
+    @GetMapping(value = "/testrecipes")
+    public List<Object> GetRecipesTest(){
+        String url= "https://api.spoonacular.com/recipes/findByNutrients?maxCalories=2000&number=7&apiKey=972014c421004f89861d4cf91103d355";
+        RestTemplate restTemplate = new RestTemplate();
+        Object[] recipes = restTemplate.getForObject(url, Object[].class);
+        return Arrays.asList(recipes);
+    }
+
+//    @PostMapping(value = "/addrecipes")
 
     // View recipe
     @GetMapping("/recipes")
